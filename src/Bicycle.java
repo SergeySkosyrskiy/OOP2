24a24f5c29d1e0635ed9047cfa4820b2cc9549b2
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-03-17 21:28:14 +0800

    Исправленая версия

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,17 @@
-public class Bicycle {
-
-    public String modelName;
-    public int wheelsCount;
+public class Bicycle extends Test implements Testable {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void service() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
     }
 }
